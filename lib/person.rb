2ceb7fa7d1f31151e4fe95e31f473ef9741af271
Ashlen <ashmencia0103@gmail.com>
2020-12-29 21:31:16 -0500

    commit tree ef34013950b29734540e4f9ef1d68031cc833691

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,17 @@
 # your code goes here
+class Person 
+    attr_reader :name, :bank_account 
+    def initialize(name)
+        @name = name 
+        @bank_account = bank_account
+    end 
+    def name
+        @name 
+    end 
+    def bank_account
+        original_amount = 25
+
+    end 
+end 
+
+name = Person.new("Stella")
